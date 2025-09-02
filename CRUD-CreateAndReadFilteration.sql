78d27d509d3102cc4161ecb050e9766b95354ca7
LivinkumarS <001liv100@ggmail.com>
2025-09-02 12:57:02 +0530

    Update and Delete

--- CRUD-CreateAndReadFilteration.sql
+++ CRUD-CreateAndReadFilteration.sql
@@ -19,4 +19,12 @@ select * from employee_list where age NOT IN (34,546,45,52,41,14,42,75);
 select * from employee_list where employeeName like '%h__u%' or age>50;
 
 
+use employees_record;
 
+select * from employee_list order by age asc;
+
+set sql_safe_updates=1;
+
+update employee_list set age=70;
+
+delete from employee_list where id=7;
